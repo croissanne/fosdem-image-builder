cec73cebc269b0d45d3674a4823737e43300f515
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2024-02-03 18:02:17 +0100

    slides: don't run in kiosk mode

--- slides/run.sh
+++ slides/run.sh
@@ -3,6 +3,6 @@
 reveal-md ./slides.md --theme serif --watch --css ./style.css --disable-auto-open &
 REVEAL_PID=$!
 
-ff -P fosdem --kiosk localhost:1948/slides.md
+ff -P fosdem localhost:1948/slides.md
 
 kill $REVEAL_PID
